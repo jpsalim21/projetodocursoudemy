429ed913e6f46c5238e3cd209ebaeb7c3879fae6
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2025-05-04 17:17:54 -0300

    Animação do player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,28 +5,40 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+var animationLock : bool = false
+
+func _ready() -> void:
+	sprite.animation_finished.connect(terminouAnimacao)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
+	if Input.is_action_just_pressed("Pular") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	var direction := Input.get_axis("Esquerda", "Direita")
+	velocity.x = direction * SPEED
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
 		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	elif animationLock:
+		velocity.x = 0
 	
 	updateSprite()
 	move_and_slide()
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Atacar"):
+		animationLock = true
+		sprite.play("Attack1")
+
+func terminouAnimacao():
+	animationLock = false
+
 func updateSprite():
+	if animationLock:
+		return
+	
 	if velocity.x > 0:
 		sprite.flip_h = false
 	elif velocity.x < 0:
@@ -44,4 +56,3 @@ func updateSprite():
 		return
 	
 	sprite.play("Idle")
-	pass
